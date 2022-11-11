d216ccfcaaa836baf7df57486f378d4a37c09a4e
Sumit Barua <sumitbarua14@gmail.com>
2022-11-11 19:01:15 +0600

    first commit

---
+++ 1clickbackend.bat
@@ -0,0 +1,25 @@
+@echo off
+start cmd /c npm init --y
+start cmd /c npm i express nodemon jsonwebtoken mongoose express-rate-limit hpp xss-clean dotenv
+type nul > index.js
+type nul > app.js
+type nul > config.env
+mkdir src
+cd src
+mkdir controllers
+cd controllers
+type nul > ProfileController.js
+type nul > LoginController.js
+type nul > ToDoListController.js
+cd ..
+mkdir middleware
+cd middleware
+type nul > AuthVerifyMiddleware.js
+cd ..
+mkdir models
+mkdir routes
+cd routes
+type nul > api.js
+cd ..
+
+
